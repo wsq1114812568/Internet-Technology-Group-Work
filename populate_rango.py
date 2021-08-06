b8d762c276519b629199b8260963cf238153bf85
wsq <1114812568@qq.com>
2021-08-06 22:31:26 +0800

    delete files in migrations folder, add additional content in requirement.txt

--- populate_rango.py
+++ populate_rango.py
@@ -11,47 +11,71 @@ def populate():
     python_pages=[
         {'title':'Official Python Tutorial',
         'url':'http://docs.python.org/3/tutorial/',
-        'views':10},
+        'likeNumber':0,
+        'views':10,
+         },
         {'title':'How to Think like a Computer Scientist',
         'url':'http://www.greenteapress.com/thinkpython',
-        'views':8},
+        'likeNumber':0,
+        'views':8,
+         },
         {'title':'Learn Python in 10 Minutes',
         'url':'http://www.korokithakis.net/tutorials/python/',
-        'views':13}]
+        'likeNumber':0,
+        'views':13,
+         }]
     
     django_pages = [ 
         {'title':'Official Django Tutorial',
         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
-        'views':16},
+        'likeNumber':0,
+        'views':16,
+        },
         {'title':'Django Rocks',
         'url':'http://www.djangorocks.com/',
-        'views':23},
+        'likeNumber':0,
+        'views':23,
+         },
         {'title':'How to Tango with Django',
         'url':'http://www.tangowithdjango.com/',
-        'views':19} ]
+        'likeNumber':0,
+        'views':19,
+         } ]
 
     other_pages = [
         {'title':'Bottle',
         'url':'http://bottlepy.org/docs/dev/',
-        'views':96},
+        'likeNumber':0,
+        'views':96,
+         },
         {'title':'Flask',
         'url':'http://flask.pocoo.org',
-        'views':26} ]
+        'likeNumber':0,
+        'views':26,
+         } ]
     java_page=[
         {'title':'JavaApi',
         'url':'https://www.matools.com/api/java8',
-        'views':85},
+        'likeNumber':0,
+        'views':85,
+         },
         {'title':'JDBC',
         'url':'https://docs.oracle.com/javase/8/docs/technotes/guides/jdbc/',
-        'views':72},
+        'likeNumber':0,
+        'views':72,
+        },
     ]
     c_page=[
         {'title':'C++',
         'url':'http://www.cplusplus.com/',
-        'views':92},
+        'likeNumber':0,
+        'views':92,
+         },
         {'title':'cppreference',
         'url':'https://en.cppreference.com/w/',
-        'views':66},
+        'likeNumber':0,
+        'views':66,
+         },
     ]
 
     cats = {'Python': {'pages': python_pages,'views':128,'likes':64},
@@ -63,16 +87,17 @@ def populate():
     for cat, cat_data in cats.items():
         c=add_cat(cat,views=cat_data['views'],likes=cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c,p['title'],p['url'],views=p['views'])
+            add_page(c,p['title'],p['url'],p['likeNumber'],views=p['views'],)
     
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print(f'-{c}:{p}')
 
-def add_page(cat,title,url,views=0):
+def add_page(cat,title,url,likeNumber,views=0):
     p=Page.objects.get_or_create(category=cat,title=title)[0]
     p.url=url
     p.views=views
+    p.likeNumber=likeNumber
     p.save()
     return p
 
@@ -82,8 +107,6 @@ def add_cat(name,views=0,likes=0):
     c.likes=likes
     c.save()
     return c
-
-
 
 if __name__=='__main__':
     print('Starting Rango population script...')
